08ea8b033c671fd181e3088986e930fbeab36af5
shamim71 <shamim71@1657e276-a2d9-11de-89ee-f3da9a46d971>
2009-09-16 17:34:25 +0000

    Initial import

---
+++ JavaSource/com/intrigueit/myc2i/story/dao/StoryDao.java
@@ -0,0 +1,9 @@
+package com.intrigueit.myc2i.story.dao;
+
+import com.intrigueit.myc2i.common.dao.GenericDao;
+import com.intrigueit.myc2i.story.domain.MemberStory;
+
+
+public interface StoryDao extends GenericDao<MemberStory,Long>{
+
+}
---
+++ JavaSource/com/intrigueit/myc2i/story/service/StoryService.java
@@ -0,0 +1,20 @@
+package com.intrigueit.myc2i.story.service;
+
+import java.util.List;
+
+
+import com.intrigueit.myc2i.story.domain.MemberStory;
+
+public interface StoryService {
+    public void save(MemberStory entity);
+
+    public void delete(MemberStory entity);
+
+	public void update(MemberStory entity);
+	
+	public MemberStory findById( Long id);
+
+	public List<MemberStory> findAll();
+	
+	public List<MemberStory> findMyAllStories(Long memberId);
+}
