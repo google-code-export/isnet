7b5d1f711ddf71d86d0e54f389c0ebd0e40d297e
shamim71 <shamim71@1657e276-a2d9-11de-89ee-f3da9a46d971>
2009-09-16 17:43:16 +0000

    Initial import

---
+++ trunk/JavaSource/com/intrigueit/myc2i/udvalues/service/UDValuesService.java
@@ -0,0 +1,18 @@
+package com.intrigueit.myc2i.udvalues.service;
+// default package
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.intrigueit.myc2i.udvalues.domain.UserDefinedValues;
+
+
+public interface UDValuesService {
+  public List<UserDefinedValues> loadAll();
+  public UserDefinedValues loadById(Long recordId);
+  public void updateUDValues (UserDefinedValues udValues);
+  public void addUDValues (UserDefinedValues udValues);
+  public void deleteUDValues(Long recordId);
+  public ArrayList<String> getCategories();
+  public List<UserDefinedValues> findByProperty(String propertyName, Object value);
+}
