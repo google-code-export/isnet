2ecd1b8b213787fd0b2e6d9621e96077475cb276
shamim71 <shamim71@1657e276-a2d9-11de-89ee-f3da9a46d971>
2009-09-16 17:58:45 +0000

    Initial import

---
+++ WebContent/js/common.js
@@ -0,0 +1,37 @@
+var Common = new Object();
+var isIE = (navigator.userAgent.indexOf("MSIE") >= 0);
+var childWindows = [];
+var styleClasses = {"left":"left_class","right":"right_class","ico":"ico_class","txt":"txt_class","dicon":"ico_class"};
+
+var cssRules = new Array();
+if (isIE) {
+  cssRules = document.styleSheets[0].rules;
+} else {
+  cssRules = document.styleSheets[0].cssRules;
+}
+
+Common.openModalPanel = function(panelId,errMag) {
+	if ( errMag ) return;
+	if( panelId ) {
+	//Richfaces.showModalPanel(panelId,{width:100,height:100,top:50});
+    Richfaces.showModalPanel(panelId,{width:400, top:25});
+  }
+ }
+
+Common.closeModalPanel = function(errMag,panelId) {
+  if(!Common.ajaxReqContainsErrs(errMag,panelId)) {
+    Richfaces.hideModalPanel(panelId);
+  }
+}
+
+Common.ajaxReqContainsErrs = function(errMag,panelId) {
+  var maxSeverityId = panelId + "HidErrForm:" + panelId + "MaxSeverity";
+  var maxSeverityObj = document.getElementById(maxSeverityId);
+  if(maxSeverityObj) {
+    var maxSeverity =  maxSeverityObj.value;
+    if(maxSeverity || errMag) {
+      return true;
+    }
+  }
+  return false;
+}
---
+++ WebContent/js/jquery/jqconfig.js
@@ -0,0 +1,77 @@
+/*#FF6A06
+jQuery.extend(jQuery.blockUI.defaults.elementMessageCSS, { width:'150px', padding:'0px', textAlign:'left', fontWeight: 'bold', color:'white', background:'red'});
+// styles for the displayBox
+jQuery.extend(jQuery.blockUI.defaults.displayBoxCSS, { width: '400px', height: '400px', top:'10%', left:'10%' });
+---- remove up code -------------------
+Configuration for jquery drag-ndrop and blockUI
+*/
+jQuery.noConflict();
+jQuery.extend(jQuery.blockUI.defaults.overlayCSS, { opacity: '0', cursor:'normal'});
+jQuery.blockUI.defaults.elementMessage = " Loading...";
+jQuery.blockUI.defaults.fadeTime = 0;
+jQuery.blockUI.defaults.fadeOut = 0;
+jQuery.extend(jQuery.blockUI.defaults.elementMessageCSS, { height:'20px',width:'100px', padding:'1px', textAlign:'left',fontWeight: 'normal', color:'white',
+                                                           fontFamily: "Arial,Helvetica,sans-serif",background:'red'});
+// styles for the displayBox
+jQuery.extend(jQuery.blockUI.defaults.displayBoxCSS, { width: '400px', height: '400px', top:'10%', left:'10%' });
+
+function forceBlockUI(msg, divClass){
+
+	// show the msg if it sent as param
+	if (typeof msg != 'undefined' ){
+		if ( msg.length > 0){
+			jQuery.blockUI.defaults.elementMessage = msg;
+		}
+	}
+	var blockDivClass = "div.mainBlock";
+	if (typeof divClass != 'undefined' ){
+		if ( divClass.length > 0){
+			blockDivClass = divClass;
+		}
+	}
+	jQuery(blockDivClass).block();
+	return true;
+}
+function forceUnblockUI(divClass){
+	var blockDivClass = "div.mainBlock";
+	if (typeof divClass != 'undefined' ){
+		if ( divClass.length > 0){
+			blockDivClass = divClass;
+		}
+	}
+
+	jQuery(blockDivClass).unblock();
+	return true;
+}
+
+function forceSBlockUI(msg, divClass) {
+  // show the msg if it sent as param
+  jQuery.extend(jQuery.blockUI.defaults.elementMessageCSS,
+                  { height:'18px',width:'100px', padding:'1px', textAlign:'left',fontWeight: 'normal', color:'white',
+                    fontFamily: "Arial,Helvetica,sans-serif",background:'red'});
+  if (typeof msg != 'undefined' ){
+    if ( msg.length > 0){
+      jQuery.blockUI.defaults.elementMessage = msg;
+    }
+  }
+  var blockDivClass = "div.mainSecBlock";
+  if (typeof divClass != 'undefined' ){
+    if ( divClass.length > 0){
+      blockDivClass = divClass;
+    }
+  }
+  jQuery(blockDivClass).block();
+  return true;
+}
+
+function forceSUnblockUI(divClass){
+  var blockDivClass = "div.mainSecBlock";
+  if (typeof divClass != 'undefined' ){
+    if ( divClass.length > 0){
+      blockDivClass = divClass;
+    }
+  }
+
+  jQuery(blockDivClass).unblock();
+  return true;
+}
