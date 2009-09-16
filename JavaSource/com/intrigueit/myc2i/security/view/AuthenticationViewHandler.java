b45afd7ebceed433f0ca81d6a0dccc4e7790316a
shamim71 <shamim71@1657e276-a2d9-11de-89ee-f3da9a46d971>
2009-09-16 17:34:49 +0000

    Initial import

---
+++ JavaSource/com/intrigueit/myc2i/security/view/AuthenticationViewHandler.java
@@ -0,0 +1,208 @@
+ /**
+ * @(#)AuthenticationViewHandler.java
+ *
+ * Copyright (c)  Ltd 2009
+ * 
+ */
+package com.intrigueit.myc2i.security.view;
+
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.intrigueit.myc2i.common.CommonConstants;
+import com.intrigueit.myc2i.common.Myc2iException;
+import com.intrigueit.myc2i.common.utility.CryptographicUtility;
+import com.intrigueit.myc2i.common.view.BasePage;
+import com.intrigueit.myc2i.common.view.ViewConstant;
+import com.intrigueit.myc2i.member.domain.Member;
+import com.intrigueit.myc2i.member.service.MemberService;
+
+@Component("authenticationViewHandler")
+@Scope("session")
+public class AuthenticationViewHandler extends BasePage implements Serializable {
+
+	/**
+	 * Serialized version no
+	 */
+	private static final long serialVersionUID = 6151863188013949687L;
+	private MemberService memberService;
+	
+
+	/**
+	 * User password
+	 */
+	private String password;
+	/**
+	 * User Login id.
+	 */
+	private String userEmailId;
+	
+	/***/
+	private String errMessage;
+	
+	private Boolean isLogged;
+	
+	
+	
+	/**
+	 * @param memberService
+	 */
+	@Autowired
+	public AuthenticationViewHandler(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	public String loginUser(){
+		String navOutCome = "";
+		try{
+			log.debug("Looging user...");
+			Member member  = this.getValidUser();
+			if(member != null){
+				//this.getSession().setAttribute(CommonConstants.CURRENT_MEMMBER, member);
+				this.setMemberinSession(member);
+				navOutCome = this.getHomePageAddress(member);
+			}
+			else{
+				this.errMessage = this.getText("lgoin_page_login_error");
+			}
+		}
+		catch(Myc2iException mEx){
+			this.errMessage = mEx.getMessage();
+		}
+		catch(Exception ex){
+			this.errMessage = this.getText("lgoin_page_login_system_error");
+			ex.printStackTrace();
+			log.error(ex.getStackTrace());
+		}
+		return navOutCome;
+	}
+	/**
+	 * Logout the User from application and clear
+	 * user information from session object.
+	 * @return
+	 * @throws Exception
+	 */
+	public String logOutUser()  {
+		try{
+			log.debug("Log out user");
+
+			HttpSession session = getSession();
+			session.removeAttribute(CommonConstants.SESSION_MEMBER_KEY);
+			session.removeAttribute(CommonConstants.SESSION_MEMBER_EMAIL);
+			session.invalidate();
+			//this.getResponse().sendRedirect(this.getRequest().getContextPath() + "/login.jsf");
+			
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return ViewConstant.BACK_TO_LOGIN;
+
+	}	
+	private void setMemberinSession(Member member) {
+  		HttpSession session =  getRequest().getSession(true);
+  		
+		session.setAttribute(CommonConstants.SESSION_MEMBER_KEY, member);
+		session.setAttribute(CommonConstants.SESSION_MEMBER_EMAIL, member.getEmail());
+	  }
+	
+	/**
+	 * Verify the User credentials during the login
+	 * process.
+	 * @return
+	 * @throws Exception
+	 */
+	private Member getValidUser() throws Exception{
+		String emailId = this.getUserEmailId();
+		String pass = this.getPassword();
+		Member member = null;
+		if(emailId.equals("") || pass.equals("")){
+			return null;
+		}
+		member = this.memberService.findByProperty("email", emailId).get(0);
+		if(member == null){
+			return null;
+		}
+		CryptographicUtility crpUtil = new CryptographicUtility();
+		String decPass = crpUtil.getDeccryptedText(member.getPassword());
+		if(!pass.equals(decPass)){
+			return null; 
+		}
+		return member;
+	}
+	/**
+	 * */
+	public String getHomePageAddress(Member member){
+		if(isFirstTimeLogin(member)){
+			return ViewConstant.OUT_COME_PASSWORD_CHANGE;
+		}
+		else{
+			return ViewConstant.TO_MENTOR_DASHBOARD;
+		}
+
+	}
+	/**
+	 * Check if the record created date and last updated date is same
+	 * to find the first time login 
+	 * */
+	public Boolean isFirstTimeLogin(Member member){
+		
+		Calendar calRecCreated = Calendar.getInstance();
+		calRecCreated.setTime(member.getRecordCreate());
+		
+		Calendar calRecLastUpdated = Calendar.getInstance();
+		calRecLastUpdated.setTime(member.getLastUpdated());
+		if(calRecCreated.equals(calRecLastUpdated)){
+			return true;
+		}
+		return false;
+	}
+
+	public String recoverPassword(){
+		return ViewConstant.TO_PASSWORD_RECOVERY;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserEmailId() {
+		return userEmailId;
+	}
+
+	public void setUserEmailId(String userEmailId) {
+		this.userEmailId = userEmailId;
+	}
+
+	public String getErrMessage() {
+		return errMessage;
+	}
+
+	public void setErrMessage(String errMessage) {
+		this.errMessage = errMessage;
+	}
+
+	public Boolean getIsLogged() {
+		if(this.getSession().getAttribute(CommonConstants.SESSION_MEMBER_EMAIL) == null){
+			return false;
+		}
+		return true;
+	}
+
+	public void setIsLogged(Boolean isLogged) {
+		this.isLogged = isLogged;
+	}
+	
+	
+	
+}
---
+++ JavaSource/com/intrigueit/myc2i/security/view/ChangePasswordViewHandler.java
@@ -0,0 +1,242 @@
+package com.intrigueit.myc2i.security.view;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.faces.model.SelectItem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.intrigueit.myc2i.common.CommonConstants;
+import com.intrigueit.myc2i.common.Myc2iException;
+import com.intrigueit.myc2i.common.utility.CryptographicUtility;
+import com.intrigueit.myc2i.common.view.BasePage;
+import com.intrigueit.myc2i.common.view.CommonValidator;
+import com.intrigueit.myc2i.common.view.ViewConstant;
+import com.intrigueit.myc2i.common.view.ViewDataProvider;
+import com.intrigueit.myc2i.member.domain.Member;
+import com.intrigueit.myc2i.member.service.MemberService;
+import com.intrigueit.myc2i.utility.Emailer;
+
+@Component("changePassViewHandler")
+@Scope("session")
+public class ChangePasswordViewHandler extends BasePage  implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private MemberService memberService;
+	
+
+	private ViewDataProvider vwdataProvider;
+	
+	private String securityQuestion1;
+	private String securityQuestion2;
+	
+	private String securityQuestionAns1;
+	private String securityQuestionAns2;
+	
+	private String oldPassword;
+	private String newPassword;
+	private String confirmPassword;
+	
+	private ArrayList<SelectItem> question1List; 
+	private ArrayList<SelectItem> question2List;
+	
+	
+	@Autowired
+	public ChangePasswordViewHandler(MemberService memberService) {
+		super();
+		this.memberService = memberService;
+	}
+
+	/**
+	 * Change user password
+	 * @return
+	 */
+	public String changeMemberPassword(){
+		try{
+			this.errMsgs.clear();
+			Member member = this.getMember();
+			CryptographicUtility crp = new CryptographicUtility();
+			this.checkOldPassword(crp.getDeccryptedText(member.getPassword()));
+			this.validate();
+			if(this.errMsgs.size()>0){
+				this.hasError = true;
+				return "";
+			}			
+			Member dbMember = this.memberService.findById(member.getMemberId());
+			this.modifyMember(dbMember);
+			this.sendConfirmationEmail(dbMember.getEmail(), newPassword);
+			return ViewConstant.PASSWORD_CHANGE_TO_HOME;
+		}
+		catch(Myc2iException ex){
+			this.errMsgs.add(ex.getMessage());
+			log.error(ex.getMessage());
+		}		
+		catch(Exception ex){
+			this.errMsgs.add(this.getText("change_password_unable_to_update"));
+			log.error(ex.getStackTrace());
+		}
+		return "";
+	}
+	
+	/** Send confirmation email to member */
+	private void sendConfirmationEmail(String email, String password)throws Exception{
+		
+		String msgBody = this.getText("email_password_change_confirmation_body", new String[]{email,password});
+		String emailSubject = this.getText("email_password_change_confirmation_subject");
+		/**Send email notification */
+		Emailer emailer = new Emailer(email, msgBody,emailSubject);
+		emailer.setContentType("text/html");
+		emailer.sendEmail();		
+	}	
+	/**
+	 * Update the member object with new information
+	 * @param member
+	 * @throws Exception
+	 */
+	private void modifyMember(Member member)throws Exception{
+		CryptographicUtility crp = new CryptographicUtility();
+		member.setLastUpdated(new Date());
+		member.setPassword(crp.getEncryptedText(this.newPassword));
+		member.setSecurityQuestion1(this.getSecurityQuestion1());
+		member.setSecurityQuestion2(this.getSecurityQuestion2());
+		member.setSecurityQuestionAns1(this.getSecurityQuestionAns1());
+		member.setSecurityQuestionAns2(this.getSecurityQuestionAns2());
+		this.memberService.save(member);
+	}
+	public void checkOldPassword(String actualPass){
+		if(!this.oldPassword.equals(actualPass)){
+			this.errMsgs.add( this.getText("change_password_change_validation_old_password_invalid"));
+		}
+	}
+	public void validate(){
+		if(CommonValidator.isEmpty(this.newPassword)){
+			this.errMsgs.add( this.getText("change_password_change_validation_new_password_empty"));
+		}
+		if(!this.newPassword.equals(this.confirmPassword)){
+			this.errMsgs.add( this.getText("change_password_dont_match"));
+		}
+		if(CommonValidator.isEmpty(this.securityQuestion1)){
+			this.errMsgs.add( this.getText("change_password_security_question1_chose"));
+		}		
+		if(CommonValidator.isEmpty(this.securityQuestionAns1)){
+			this.errMsgs.add( this.getText("change_password_security_question1_empty"));
+		}	
+		if(CommonValidator.isEmpty(this.securityQuestion2)){
+			this.errMsgs.add( this.getText("change_password_security_question2_chose"));
+		}		
+		if(CommonValidator.isEmpty(this.securityQuestionAns2)){
+			this.errMsgs.add( this.getText("change_password_security_question2_empty"));
+		}	
+	}
+	
+	public String getSecurityQuestion1() {
+		return securityQuestion1;
+	}
+
+
+	public void setSecurityQuestion1(String securityQuestion1) {
+		this.securityQuestion1 = securityQuestion1;
+	}
+
+
+	public String getSecurityQuestion2() {
+		return securityQuestion2;
+	}
+
+
+	public void setSecurityQuestion2(String securityQuestion2) {
+		this.securityQuestion2 = securityQuestion2;
+	}
+
+
+	public String getSecurityQuestionAns1() {
+		return securityQuestionAns1;
+	}
+
+
+	public void setSecurityQuestionAns1(String securityQuestionAns1) {
+		this.securityQuestionAns1 = securityQuestionAns1;
+	}
+
+
+	public String getSecurityQuestionAns2() {
+		return securityQuestionAns2;
+	}
+
+
+	public void setSecurityQuestionAns2(String securityQuestionAns2) {
+		this.securityQuestionAns2 = securityQuestionAns2;
+	}
+
+
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public ArrayList<SelectItem> getQuestion1List() {
+		if(question1List == null){
+			this.question1List = this.getVwdataProvider().getQuestionList();
+		}
+		return question1List;
+	}
+
+	public void setQuestion1List(ArrayList<SelectItem> question1List) {
+		this.question1List = question1List;
+	}
+
+	public ArrayList<SelectItem> getQuestion2List() {
+		if(question2List == null){
+			this.question2List = this.getVwdataProvider().getQuestionList();
+		}
+		return question2List;
+	}
+
+	public void setQuestion2List(ArrayList<SelectItem> question2List) {
+		this.question2List = question2List;
+	}
+
+	public ViewDataProvider getVwdataProvider() {
+		if(vwdataProvider == null){
+			vwdataProvider= new ViewDataProvider();
+		}
+		return vwdataProvider;
+	}
+
+	public void setVwdataProvider(ViewDataProvider vwdataProvider) {
+		this.vwdataProvider = vwdataProvider;
+	}
+	
+
+}
