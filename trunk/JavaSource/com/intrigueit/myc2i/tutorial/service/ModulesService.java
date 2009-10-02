edd7b205f83cddce148b7bece103492c5e6d541b
shamim71 <shamim71@1657e276-a2d9-11de-89ee-f3da9a46d971>
2009-10-02 20:24:29 +0000

    File formated

--- trunk/JavaSource/com/intrigueit/myc2i/tutorial/service/ModulesService.java
+++ trunk/JavaSource/com/intrigueit/myc2i/tutorial/service/ModulesService.java
@@ -1,4 +1,5 @@
 package com.intrigueit.myc2i.tutorial.service;
+
 // default package
 
 import java.util.ArrayList;
@@ -7,14 +8,23 @@ import java.util.List;
 import com.intrigueit.myc2i.tutorial.domain.TestTutorialModules;
 
 public interface ModulesService {
-  public List<TestTutorialModules> loadAll();
-  public TestTutorialModules loadById(Long recordId);
-  public void updateModules (TestTutorialModules modules);
-  public void addModules (TestTutorialModules modules);
-  public TestTutorialModules loadById();
-  public void deleteModules(Long recordId);
-  public ArrayList<String> getCategories();
-  public List<TestTutorialModules> findByProperty(String propertyName, Object value);
-  public List<TestTutorialModules> findByProperties(String docTypeId, String userId);
-  public boolean isModuleExist(Long recordId,Long docId,String moduleName);
+	public List<TestTutorialModules> loadAll();
+
+	public TestTutorialModules loadById(Long recordId);
+
+	public void updateModules(TestTutorialModules modules);
+
+	public void addModules(TestTutorialModules modules);
+
+	public void deleteModules(Long recordId);
+
+	public ArrayList<String> getCategories();
+
+	public List<TestTutorialModules> findByProperty(String propertyName,
+			Object value);
+
+	public List<TestTutorialModules> findByProperties(String docTypeId,
+			String userId);
+
+	public boolean isModuleExist(Long recordId, Long docId, String moduleName);
 }
