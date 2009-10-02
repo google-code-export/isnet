4410517d3f1cea85e35ce368697039f454c57896
shamim71 <shamim71@1657e276-a2d9-11de-89ee-f3da9a46d971>
2009-10-02 20:21:19 +0000

    File formated

--- trunk/JavaSource/com/intrigueit/myc2i/tutorial/service/QuestionAnsService.java
+++ trunk/JavaSource/com/intrigueit/myc2i/tutorial/service/QuestionAnsService.java
@@ -1,4 +1,5 @@
 package com.intrigueit.myc2i.tutorial.service;
+
 // default package
 
 import java.util.ArrayList;
@@ -6,16 +7,28 @@ import java.util.List;
 import com.intrigueit.myc2i.tutorial.domain.TestTutorialQuestionAns;
 
 public interface QuestionAnsService {
-  public List<TestTutorialQuestionAns> loadAll();
-  public TestTutorialQuestionAns loadById(Long recordId);
-  public void updateQuestionAns (TestTutorialQuestionAns questionAns);
-  public void addQuestionAns (TestTutorialQuestionAns questionAns);
-  public TestTutorialQuestionAns loadById();
-  public void deleteQuestionAns(Long recordId);
-  public ArrayList<String> getCategories();
-  public List<TestTutorialQuestionAns> findByProperty(String propertyName, Object value);
-  public List<TestTutorialQuestionAns> findByProperties(String moduleId);
-  public Long getMaxPageNo(Long moduleId);
-  public boolean isQuestionExist(Long recordId,Long moduleId,String question);
-  public boolean isPageNoExist(Long recordId,Long moduleId,Long pageNo);
+	public List<TestTutorialQuestionAns> loadAll();
+
+	public TestTutorialQuestionAns loadById(Long recordId);
+
+	public void updateQuestionAns(TestTutorialQuestionAns questionAns);
+
+	public void addQuestionAns(TestTutorialQuestionAns questionAns);
+
+	public TestTutorialQuestionAns loadById();
+
+	public void deleteQuestionAns(Long recordId);
+
+	public ArrayList<String> getCategories();
+
+	public List<TestTutorialQuestionAns> findByProperty(String propertyName,
+			Object value);
+
+	public List<TestTutorialQuestionAns> findByProperties(String moduleId);
+
+	public Long getMaxPageNo(Long moduleId);
+
+	public boolean isQuestionExist(Long recordId, Long moduleId, String question);
+
+	public boolean isPageNoExist(Long recordId, Long moduleId, Long pageNo);
 }
